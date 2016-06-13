1964a3833ca5d4610437192134cddfb899be5ac3
Denis Lachance <dudeofea@hotmail.com>
2016-06-13 01:49:32 -0600

    fuck dynamic loading

--- js/ide.js
+++ js/ide.js
@@ -1,5 +1,6 @@
 //to avoid leaking globals
 (function(){
+	//on page load
 	document.addEventListener("DOMContentLoaded", function(event) {
 		//thanks to http://stackoverflow.com/a/950146
 		var loadScript = function(url, callback){
@@ -15,8 +16,28 @@
 			// Fire the loading
 			head.appendChild(script);
 		};
-		//load some utilities and the actual IDE
-		loadScript(base+'/gui.js')
+		//load an html file and append into element
+		var loadHTML = function(url, elem, callback){
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function (e) {
+				if (xhr.readyState == 4 && xhr.status == 200) {
+					console.log(xhr);
+					//create wrapper, then extract
+					var wrapper = document.createElement('div');
+					wrapper.innerHTML= xhr.responseText;
+					var html = wrapper.firstChild;
+					elem.appendChild(html);
+				}
+			}
+			xhr.open("GET", url, true);
+			xhr.setRequestHeader('Content-type', 'text/html');
+			xhr.send();
+		};
+		//load views
+		loadHTML('html/side-pane.html', document.body);
+		loadHTML('html/empty-window.html', document.body);
+		//add style to body
+		document.body.className = "js-ide-right";
 	});
 	//get our script path
 	var scripts = document.getElementsByTagName("script"),
